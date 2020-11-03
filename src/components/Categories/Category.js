4732186f66bd20e30102a52fcfdd2deb3e40d8e6
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-11-03 21:36:40 +0100

    add accordion in categories to show subcategories

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SubCategoriesList from './SubCategories/SubCategoriesList'
 import './Category.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,20 +8,43 @@ import { ReactComponent as Hide } from '../../assets/ic_hide.svg'
 
 const Category = props => {
 
-    const star = <FontAwesomeIcon icon={faStar} color="#4FD053" size="1x" />
-    
+    const star = <FontAwesomeIcon icon={faStar} color="#4FD053" size="lg" />
+
+    const [show, setShow] = useState(false)
+
     return  (
         <>
-            <div className="category-info">
-                    {star}
-                    <h2>{props.name}</h2>
-                    <Hide className="hide-icon"/>
-            </div>
-
+            <div className="all">
+                <div className="category-container">
+                    <button onClick={() => setShow(!show)}>
+                        <div className="category-info">
+                                <div className="star-icon">
+                                    {star}
+                                </div>
+                                <div>
+                                    <h2 className="category-name">{props.name}{show}</h2>
+                                </div>
+                        </div>
+                        <div className="hide-icon-container">
+                            {show ? (<Hide/>) : (<Hide className="hide-icon"/>)}
+                        </div>
+                    </button>
+                </div>
+                {show 
+                ? 
+                (<div> 
                     <div className="categories">
                         <h3 className="all-section">Ver toda la sección</h3>
-                    </div>
-                    {props.categories.map(subcategories => <SubCategoriesList key={subcategories.id} name={subcategories.name}/>)}
+                    </div>    
+                    <div className="subcategories-container">
+                        {props.categories.map(subcategories => <SubCategoriesList key={subcategories.id} name={subcategories.name} />)}
+                    </div> 
+                </div>) 
+                : 
+                ('')
+                }
+  
+            </div>
         </>
     )
 }
