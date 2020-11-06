b35c3f46c56c9511fc5ce12e510145432bd0e8fb
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-11-06 13:13:19 +0100

    add favicon, and update readme

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -8,7 +8,7 @@ const Spinner = () => {
             <svg className="spinner" version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="25 25 50 50">
-                <circle cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke="#45d6b5" stroke-linecap="round" stroke-dashoffset="0" stroke-dasharray="100, 200">
+                <circle cx="50" cy="50" r="20" fill="none" strokeWidth="5" stroke="#45d6b5" strokeLinecap="round" strokeDashoffset="0" strokeDasharray="100, 200">
                     <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 50 50" to="360 50 50" dur="2.5s" repeatCount="indefinite"/>
                     <animate attributeName="stroke-dashoffset" values="0;-30;-124" dur="1.25s" repeatCount="indefinite"/>
                     <animate attributeName="stroke-dasharray" values="0,200;110,200;110,200" dur="1.25s" repeatCount="indefinite"/>
